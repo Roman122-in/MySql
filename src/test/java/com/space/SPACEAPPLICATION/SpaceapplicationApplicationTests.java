23a84edf49a533f5a410f0cf55cdf9b6952f189a
yTCsjLSC <rchalowy@gmail.com>
2022-06-22 19:06:44 +0300

    v1

---
+++ src/test/java/com/space/SPACEAPPLICATION/SpaceapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space.SPACEAPPLICATION;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
